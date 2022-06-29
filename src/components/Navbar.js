9e43504a670641b7f7a90dc1d8d0d3f39f460d99
KBU <vancouver2990@gmail.com>
2022-06-29 14:21:02 +0200

    add routes and link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,30 @@
-import { Component, tags } from "@odoo/owl";
+import { Component, tags, router } from "@odoo/owl";
+const Link = router.Link;
 
 const NAVBAR_TEMPLATE = tags.xml/*xml*/ `
 <nav class="navbar navbar-light">
     <div class="container">
-        <a class="navbar-brand" href="index.html">conduit</a>
+        <!-- <a class="navbar-brand" href="index.html">conduit</a> -->
+        <Link to="'HOME'" class="navbar-brand">conduit</Link>
         <ul class="nav navbar-nav pull-xs-right">
             <li class="nav-item">
                 <!-- Add "active" class when you're on that page" -->
-                <a class="nav-link active" href="">Home</a>
+                <Link to="'HOME'" class="nav-link">Home</Link>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="">
-                    <i class="ion-compose"></i> New Post
-                </a>
+                <Link to="'EDITOR'" class="nav-link"><i class="ion-compose"></i> New Post</Link>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="">
-                    <i class="ion-gear-a"></i> Settings
-                </a>
+                <Link to="'SETTINGS'" class="nav-link"><i class="ion-gear-a"></i> Settings</Link>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="">Sign in</a>
+                <Link to="'LOG_IN'" class="nav-link">Sign in</Link>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="">Sign up</a>
+                <Link to="'REGISTER'" class="nav-link">Sign up</Link>
+            </li>
+            <li class="nav-item">
+                <Link to="'PROFILE'" class="nav-link">Coding Dodo</Link>
             </li>
         </ul>
     </div>
@@ -31,4 +32,5 @@ const NAVBAR_TEMPLATE = tags.xml/*xml*/ `
 `;
 export class Navbar extends Component {
   static template = NAVBAR_TEMPLATE;
+  static components = { Link };
 }
